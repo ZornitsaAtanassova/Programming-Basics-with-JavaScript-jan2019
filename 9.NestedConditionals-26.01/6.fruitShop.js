765f1c2caa6b5b442857a3a980006b1615cc189b
ZornitsaAtanassova <zornitsa.atanassova@gmail.com>
2019-03-01 16:50:20 +0200

    Fixed 6.Lab and Added 7.Lab

--- 9.NestedConditionals-26.01/6.fruitShop.js
+++ 9.NestedConditionals-26.01/6.fruitShop.js
@@ -1,13 +1,13 @@
 function fruitShop(input) {
     //Read
-    let fruit = (input.shift()).toLowerCase;
-    let weekDay = (input.shift()).toLowerCase;
+    let fruit = (input.shift()).toLowerCase();
+    let weekDay = (input.shift()).toLowerCase();
     let quantity = Number(input.shift());  
     
     //Manipulate
-    let price = 0;   
+    let price = 0;
     
-    if (weekDay === "Monday" || weekDay === "Tuesday" || weekDay === "Wednesday" || weekDay === "Thursday" || weekDay === "Friday") {
+    if (weekDay === "monday" || weekDay === "tuesday" || weekDay === "wednesday" || weekDay === "thursday" || weekDay === "friday") {
         switch (fruit) {
             case "banana":
             price = 2.50 * quantity;
@@ -48,93 +48,96 @@ function fruitShop(input) {
             console.log("error");
             break;
         }
-    } else if (weekDay === "Saturday" || weekDay === "Sunday") {    
+    } else if (weekDay === "saturday" || weekDay === "sunday") {    
         switch (fruit) {    
             case "banana":
             price = 2.7 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "apple":    
             price = 1.25 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "orange":    
             price = 0.90 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "grapefruit":    
             price = 1.60 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "kiwi":    
             price = 3.00 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "pineapple":    
             price = 5.60 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             case "grapes":    
             price = 4.20 * quantity;    
-            console.log(price.toFixed(2));    
+            console.log(price.toFixed(2));
             break;
             
             default:    
             console.log("error");    
-            break;    
-        }    
-    } else {    
-        console.log("error");    
+            break;
+        }  
+    } else {
+        console.log("error");
     }
     
     
-    // Alternative way ?????????
-    /* let price = 0;
-
-    if (weekDay === "Monday" || weekDay === "Tuesday" || weekDay === "Wednesday" || weekDay === "Thursday" || weekDay === "Friday") {
-        if (fruit == "banana") {
+    // Alternative way
+    /* if (weekDay === "monday" || weekDay === "tuesday" || weekDay === "wednesday" || weekDay === "thursday" || weekDay === "friday") {
+        if (fruit === "banana") {
             price = quantity * 2.50;
-        } else if (fruit == "apple") {
+        } else if (fruit === "apple") {
             price = quantity * 1.20;
-        } else if (fruit == "orange") {
+        } else if (fruit === "orange") {
             price = quantity * 0.85;
-        } else if (fruit == "grapefruit") {
+        } else if (fruit === "grapefruit") {
             price = quantity * 1.45;
-        } else if (fruit == "kiwi") {
+        } else if (fruit === "kiwi") {
             price = quantity * 2.70;
-        } else if (fruit == "pineapple") {
+        } else if (fruit === "pineapple") {
             price = quantity * 5.50;
-        } else if (fruit == "grapes") {
+        } else if (fruit === "grapes") {
             price = quantity * 3.85;
         } else {
             console.log("error");
+            return;
         }
         console.log(price.toFixed(2));
-    } else if (weekDay === "Saturday" || weekDay === "Sunday") {
-        if (fruit == "banana") {
-            price = quantity * 2.70
-        } else if (fruit == "apple") {
+    } else if (weekDay === "saturday" || weekDay === "sunday") {
+        if (fruit === "banana") {
+            price = quantity * 2.70;
+        } else if (fruit === "apple") {
             price = quantity * 1.25;
-        } else if (fruit == "orange") {
+        } else if (fruit === "orange") {
             price = quantity * 0.90;
-        } else if (fruit == "grapefruit") {
+        } else if (fruit === "grapefruit") {
             price = quantity * 1.60;
-        } else if (fruit == "kiwi") {
+        } else if (fruit === "kiwi") {
             price = quantity * 3.00;
-        } else if (fruit == "pineapple") {
+        } else if (fruit === "pineapple") {
             price = quantity * 5.60;
-        } else if (fruit == "grapes") {
+        } else if (fruit === "grapes") {
             price = quantity * 4.20;
         } else {
             console.log("error");
+            return;
         }
         console.log(price.toFixed(2));
+    } else {
+        console.log("error");
+        return;
     } */
 }
 
